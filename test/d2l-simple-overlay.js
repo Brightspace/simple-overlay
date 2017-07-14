1151835d47411a7fa8de346647429326be516334
Ryan Mahler <Ryan.Mahler@D2L.com>
2017-07-14 17:16:56 -0400

    Add some tests for slots and event
    Had to rejigger a few things to get the tests to work, which is a bit of a shame, but legitimately couldn't get it to work any other way. This method was shamelessly ripped from the https://github.com/PolymerElements/paper-toolbar tests.

--- test/d2l-simple-overlay.js
+++ test/d2l-simple-overlay.js
@@ -1,12 +1,12 @@
-/* global describe, it, beforeEach, fixture */
+/* global describe, it, beforeEach, fixture, expect */
 
 'use strict';
 
-describe('<d2l-simple-overlay>', function() {
-	var widget;
+describe('d2l-simple-overlay', function() {
+	var component;
 
 	beforeEach(function() {
-		widget = fixture('d2l-simple-overlay-fixture');
+		component = fixture('overlay-fixture');
 	});
 
 	it('should fire d2l-simple-overlay-closed event on closing', function(done) {
@@ -16,7 +16,7 @@ describe('<d2l-simple-overlay>', function() {
 		};
 		document.addEventListener('d2l-simple-overlay-closed', handler);
 
-		widget._handleClose();
+		component._handleClose();
 	});
 
 	it('should fire recalculate-columns event on closing', function(done) {
@@ -26,7 +26,7 @@ describe('<d2l-simple-overlay>', function() {
 		};
 		document.addEventListener('recalculate-columns', handler);
 
-		widget._handleClose();
+		component._handleClose();
 	});
 
 	it('should fire d2l-simple-overlay-opening event on opening', function(done) {
@@ -36,6 +36,44 @@ describe('<d2l-simple-overlay>', function() {
 		};
 		document.addEventListener('d2l-simple-overlay-opening', handler);
 
-		widget._renderOpened();
+		component._renderOpened();
+	});
+
+	it('should have a default header in the "header" slot', function() {
+		expect(component.$.headerContainer.querySelector('h2')).to.not.be.null;
+	});
+
+	describe('slot behavior', function() {
+		it('should allow for custom content in the "header" slot', function() {
+			var item = document.createElement('div');
+			item.setAttribute('slot', 'header');
+			Polymer.dom(component).appendChild(item);
+			Polymer.dom.flush();
+
+			expect(component.$.headerContainer.querySelector('h2')).to.be.null;
+
+			var insertionPoint = Polymer.dom(item).getDestinationInsertionPoints()[0];
+			expect(Polymer.dom(insertionPoint).parentNode).to.equal(component.$.headerContainer);
+		});
+
+		it('should have a main slot', function() {
+			var item = document.createElement('div');
+			Polymer.dom(component).appendChild(item);
+			Polymer.dom.flush();
+
+			var insertionPoint = Polymer.dom(item).getDestinationInsertionPoints()[0];
+			expect(Polymer.dom(insertionPoint).parentNode).to.equal(component.$.contentContainer);
+		});
+
+		it('should fire the d2l-simple-overlay-opening event to its children when overlay is opened', function(done) {
+			var item = document.createElement('div');
+			item.addEventListener('d2l-simple-overlay-opening', function() {
+				done();
+			});
+			Polymer.dom(component).appendChild(item);
+			Polymer.dom.flush();
+
+			component.open();
+		});
 	});
 });

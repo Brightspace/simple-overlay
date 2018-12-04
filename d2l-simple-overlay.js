94eb7805ea51557ffb673a81f38f89301d63c788
Dave Lockhart <dave.lockhart@d2l.com>
2018-12-04 18:37:45 -0500

    Polymer 3 Conversion

--- d2l-simple-overlay.js
+++ d2l-simple-overlay.js
@@ -8,15 +8,15 @@ Polymer-based web component for a simple overlay.  This overlay supports using d
   from HTML and may be out of place here. Review them and
   then delete this comment!
 */
-import '../@polymer/polymer/polymer-legacy.js';
+import '@polymer/polymer/polymer-legacy.js';
 
-import { IronOverlayBehavior } from '../@polymer/iron-overlay-behavior/iron-overlay-behavior.js';
-import '../d2l-icons/d2l-icons.js';
-import '../d2l-polymer-behaviors/d2l-dom.js';
+import { IronOverlayBehavior } from '@polymer/iron-overlay-behavior/iron-overlay-behavior.js';
+import 'd2l-icons/d2l-icons.js';
+import 'd2l-polymer-behaviors/d2l-dom.js';
 import './d2l-simple-overlay-close-button.js';
 import './d2l-simple-overlay-styles.js';
-import { Polymer } from '../@polymer/polymer/lib/legacy/polymer-fn.js';
-import { dom } from '../@polymer/polymer/lib/legacy/polymer.dom.js';
+import { Polymer } from '@polymer/polymer/lib/legacy/polymer-fn.js';
+import { dom } from '@polymer/polymer/lib/legacy/polymer.dom.js';
 const $_documentContainer = document.createElement('template');
 
 $_documentContainer.innerHTML = `<dom-module id="d2l-simple-overlay">
@@ -127,11 +127,9 @@ Polymer({
 		document.body.style.overflow = 'hidden';
 	},
 	_updateBackdropStyle: function() {
-		var customStyle = document.createElement('custom-style');
-		var style = document.createElement('style');
+		var style = document.createElement('style', 'custom-style');
 		style.innerHTML = 'iron-overlay-backdrop { --iron-overlay-backdrop-opacity: 0.0; }';
-		customStyle.appendChild(style);
-		document.body.appendChild(customStyle);
+		document.body.appendChild(style);
 	},
 	get scrollRegion() {
 		return dom(this.root).querySelector('.scrollable');

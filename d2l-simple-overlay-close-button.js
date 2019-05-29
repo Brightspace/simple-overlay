e20e03dce20752671f9e8b5686a9ee4d1bb7f098
Stacey Van Herk <Stacey.VanHerk@D2L.com>
2019-05-29 10:58:06 -0400

    As part of the WCAG 2.1 work, change this to a d2l-button-icon so it has the new hover and focus effects

--- d2l-simple-overlay-close-button.js
+++ d2l-simple-overlay-close-button.js
@@ -3,28 +3,28 @@
 Polymer-based web component for the simple overlay close button.
 @demo demo/simple-overlay-close-button.html Simple Overlay Close Button
 */
-/*
-  FIXME(polymer-modulizer): the above comments were extracted
-  from HTML and may be out of place here. Review them and
-  then delete this comment!
-*/
+
 import '@polymer/polymer/polymer-legacy.js';
 
-import 'd2l-icons/d2l-icons.js';
-import './d2l-simple-overlay-close-button-styles.js';
+import 'd2l-button/d2l-button-icon.js';
 import { Polymer } from '@polymer/polymer/lib/legacy/polymer-fn.js';
 const $_documentContainer = document.createElement('template');
 
 $_documentContainer.innerHTML = `<dom-module id="d2l-simple-overlay-close-button">
 	<template strip-whitespace="">
-		<style include="d2l-simple-overlay-close-button-styles"></style>
-		<button aria-label$="[[closeText]]" class="close-button" on-tap="_onTap">
-			<slot>
-				<d2l-icon icon="d2l-tier1:close-large-thick"></d2l-icon>
-			</slot>
-		</button>
+		<style>
+			.d2l-simple-overlay-close {
+				padding: 0 0 0.2rem 0;
+			}
+			@media screen and (-ms-high-contrast: active), (-ms-high-contrast: none) {
+				.d2l-simple-overlay-close {
+					padding-top: 24px;
+				}
+			}
+		</style>
+		<d2l-button-icon icon="d2l-tier1:close-large-thick" text="[[closeText]]" class="d2l-simple-overlay-close" on-tap="_onTap"></d2l-button-icon>
 	</template>
-	
+
 </dom-module>`;
 
 document.head.appendChild($_documentContainer.content);
